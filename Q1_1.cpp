c7f6c6aeee1c94f4bfa302f8ae7c8dca3d5b7762
petermaier <brotkrumen@gmx.de>
2014-12-18 14:39:28 +0100

    Update Q1_1.cpp

--- Q1_1.cpp
+++ Q1_1.cpp
@@ -2,8 +2,6 @@
 #include <string.h>
 using namespace std;
 
-/* 1.1 Implement an algorithm to determine if a string has all unique characters. What
-if you cannot use additional data structures? */
 bool Q1_1(string str)
 {
 	for(int i=0; i<str.length(); i++)
