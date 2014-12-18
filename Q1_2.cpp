fa7cca7a75ec1c9b6d517abeae4999bc746703ae
petermaier <brotkrumen@gmx.de>
2014-12-18 14:39:41 +0100

    Update Q1_2.cpp

--- Q1_2.cpp
+++ Q1_2.cpp
@@ -2,8 +2,6 @@
 #include <string.h>
 using namespace std;
 
-/* 1.2 Implement a function void reverse(char* str) in C or C++ which reverses a nullterminated
-string. */
 void Q1_2(char *str)
 {
 	const size_t len = strlen(str);
